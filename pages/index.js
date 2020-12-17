83111ecb6631305d3f82a0fc0e6a7edb11738898
TM <923727421@qq.com>
2020-12-17 15:23:00 +0800

    ~

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import "./index.less";
+// import "./index.less";
 import LayoutDefault from "@/layout/default";
 import Link from 'next/link';
 import { useSelector } from "react-redux";
@@ -10,6 +10,24 @@ function ArticleItem({ title, description, id }) {
             <a className='art'>
                 <p className='tit'>{title}</p>
                 <p>{description}</p>
+                <style jsx>{`
+                      .art{
+                        display: block;
+                        cursor:pointer;
+                        border-radius:8px;
+                        border:1px solid #eeeeee;
+                        padding:30px 15px;
+                        margin:15px;
+                        transition:all .25s linear;
+                        
+                    }
+                    .art:hover{
+                        box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
+                    }
+                    .art .tit{
+                        font-size:20px;
+                    }
+                `}</style>
             </a>
         </Link >
     )
@@ -26,6 +44,8 @@ function Home() {
                         return <ArticleItem key={item} title={item.title} description={item.description} id={item.id} ></ArticleItem>
                     })
                 }
+
+      
             </div>
         </LayoutDefault>
     );
